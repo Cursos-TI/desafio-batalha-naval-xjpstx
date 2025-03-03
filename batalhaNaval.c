93176f99e3eb41031f137e14a795009befe81fc4
xjpstx <jpb22305@gmail.com>
2025-03-03 15:24:55 +0000

    atualização do desafio mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,101 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+// declaração das constantes
+#define LINHA_TABULEIRO 10
+#define COLUNA_TABULEIRO 10
+#define LINHA_CONE 5
+#define COLUNA_CONE 5
+#define LINHA_CRUZ 5
+#define COLUNA_CRUZ 5
+#define LINHA_OCTAEDRO 5
+#define COLUNA_OCTAEDRO 5
+#define LINHA_NAVIO 5
+#define COLUNA_NAVIO 5
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+// função para sobrepor o tabuleiro com as matrizes já definidas
+void sobreporTabuleiro(int tabuleiro[LINHA_TABULEIRO][COLUNA_TABULEIRO], int figura[LINHA_CONE][COLUNA_CONE], int inicioLinha, int inicioColuna){
+    for(int i = 0; i < LINHA_CONE; i++){
+        for(int j = 0; j < COLUNA_CONE; j++){
+            if (figura[i][j] != 0) {
+                tabuleiro[inicioLinha + i][inicioColuna + j] = figura[i][j];
+            }
+        }
+    }
+}
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+// função para imprimir o tabuleiro
+void imprimirTabuleiro(int tabuleiro[LINHA_TABULEIRO][COLUNA_TABULEIRO]){
+    for(int i = 0; i < LINHA_TABULEIRO; i++){
+        for(int j = 0; j < COLUNA_TABULEIRO; j++){
+            printf("%2d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+int main(){
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    int tabuleiro[LINHA_TABULEIRO][COLUNA_TABULEIRO] = {0}; // matriz tabuleiro 10x10 
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    int cone[LINHA_CONE][COLUNA_CONE] = { // matriz cone 5x5
+        {0, 0, 0, 0, 0},
+        {0, 0, 5, 0, 0},
+        {0, 5, 5, 5, 0},
+        {0, 0, 5, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    int cruz[LINHA_CRUZ][COLUNA_CRUZ] = { // matriz cruz 5x5
+        {0, 0, 0, 0, 0},
+        {0, 0, 5, 0, 0},
+        {5, 5, 5, 5, 5},
+        {0, 0, 5, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    int octaedro[LINHA_OCTAEDRO][COLUNA_OCTAEDRO] = { // matriz octaedro 5x5
+        {0, 0, 0, 0, 0},
+        {0, 0, 5, 0, 0},
+        {0, 5, 5, 5, 0},
+        {0, 0, 5, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    int navioHorizontal[LINHA_NAVIO][COLUNA_NAVIO] = { // matriz navioHorizontal 5x5 (construída como 3x3)
+        {0, 0, 0},
+        {3, 3, 3},
+        {0, 0, 0}
+    };
+
+    int navioVertical[LINHA_NAVIO][COLUNA_NAVIO] = { // matriz navioVertical 5x5 (construída como 3x3)
+        {0, 3, 0},
+        {0, 3, 0},
+        {0, 3, 0}
+    };
+
+    int navioDiagonal1[LINHA_NAVIO][COLUNA_NAVIO] = { // matriz navioDiagonal1 5x5 (construída como 3x3)
+        {3, 0, 0},
+        {0, 3, 0},
+        {0, 0, 3}
+    };
+
+    int navioDiagonal2[LINHA_NAVIO][COLUNA_NAVIO] = { // matriz navioDiagonal2 5x5 (construída como 3x3)
+        {0, 0, 3},
+        {0, 3, 0},
+        {3, 0, 0}
+    };
+
+    // chama as funções void e retorna para sobrepor a matriz tabuleiro através da posição inicial definida para outras matrizes
+    sobreporTabuleiro(tabuleiro, cone, 0, 0);
+    sobreporTabuleiro(tabuleiro, cruz, 3, 3);
+    sobreporTabuleiro(tabuleiro, octaedro, 6, 5);
+    sobreporTabuleiro(tabuleiro, navioHorizontal, 3, 1);
+    sobreporTabuleiro(tabuleiro, navioVertical, 2, 5);
+    sobreporTabuleiro(tabuleiro, navioDiagonal1, 7, 1);
+    sobreporTabuleiro(tabuleiro, navioDiagonal2, 0, 7);
+
+    printf("Tabuleiro após a ativação dos poderes\n");
+    imprimirTabuleiro(tabuleiro);
 
     return 0;
 }
